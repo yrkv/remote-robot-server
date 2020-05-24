0e7f7d40459fdd84d4f930073b361d8f332c1eeb
Yegor Kuznetsov <yegor@tydbits.com>
2020-05-24 16:53:06 -0700

    exceedingly simple json transfer system

--- project/urls.py
+++ project/urls.py
@@ -3,11 +3,15 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from welcome.views import index, health
+from welcome.robot_control import get_state, set_state
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    url(r'^get_state$', get_state),
+    url(r'^set_state$', set_state),
 
     url(r'^$', index),
     url(r'^health$', health),
